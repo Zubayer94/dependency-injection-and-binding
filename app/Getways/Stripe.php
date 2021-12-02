c2e05193c06211ee98e1edfc66c840c7c9bb0660
zubayer94 <zubayer94@gmail.com>
2021-12-02 11:26:57 +0600

    added initial files

---
+++ app/Getways/Stripe.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Getways;
+
+class Stripe
+{
+
+}
+
---
+++ app/Interfacres/PaymentGetwayInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Interfaces;
+
+Interface PaymentGetwayInterface
+{
+    public function pay();
+}
+
+
